5217bce6a08658da0f845c60536c9886ef30205f
amertak <firefoxmcxd009@gmail.com>
2026-05-30 15:27:46 +0700

    update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,14 @@
-// middleware/auth.js
-
 module.exports = (req, res, next) => {
   const key = req.headers["x-api-key"];
-
+  
+  console.log("API KEY RECEIVED:", key); // 🔥 debug
+  
   if (!key || key !== process.env.API_KEY) {
     return res.status(403).json({
       status: "error",
       message: "Invalid API Key"
     });
   }
-
+  
   next();
 };
